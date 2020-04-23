c7417e381aa6b3fbf08fcec86f860f579558f539
vermavinay <vinay.verma@wobot.ai>
2020-04-23 21:11:20 +0530

    added libraries to gihub

--- face/script.js
+++ face/script.js
@@ -1,10 +1,10 @@
 const video = document.getElementById('video')
 
 Promise.all([
-    faceapi.nets.tinyFaceDetector.loadFromUri('https://vermavinay982.github.io/face/models'),
-    faceapi.nets.faceLandmark68Net.loadFromUri('https://vermavinay982.github.io/face/models'),
-    faceapi.nets.faceRecognitionNet.loadFromUri('https://vermavinay982.github.io/face/models'),
-    faceapi.nets.faceExpressionNet.loadFromUri('https://vermavinay982.github.io/face/models')
+    faceapi.nets.tinyFaceDetector.loadFromUri('https://vermavinay982.github.io/face/models/'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('https://vermavinay982.github.io/face/models/'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('https://vermavinay982.github.io/face/models/'),
+    faceapi.nets.faceExpressionNet.loadFromUri('https://vermavinay982.github.io/face/models/')
 ]).then(startVideo)
 
 function startVideo(){
